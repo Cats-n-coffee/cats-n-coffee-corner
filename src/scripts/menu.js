8e2f4a6d3f3cbca0d512047551bffb456988e22d
Cats-n-coffee <lucie.macaigne@gmail.com>
2024-02-10 20:12:33 -0500

    limit content scroll on small devices when menu open

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,9 @@
 document.querySelector('.mobile-menu-open').addEventListener('click', () => {
     document.querySelector('.navbar-wrapper').classList.toggle('expanded');
+    document.body.classList.add('mobile-menu-opened');
 })
 
 document.querySelector('.mobile-menu-close').addEventListener('click', () => {
     document.querySelector('.navbar-wrapper').classList.toggle('expanded');
+    document.body.classList.remove('mobile-menu-opened');
 })
