36dc14ffea90581d49f2c50226bd90c6dd4e975d
Cats-n-coffee <68707570+Cats-n-coffee@users.noreply.github.com>
2024-06-23 16:09:53 -0400

    Sockets post (#7)
    * limit posts to three on home page, fix scroll
    
    * add learning intro and socket post

--- src/constants/content/intros.js
+++ src/constants/content/intros.js
@@ -5,5 +5,9 @@ export default {
     clarify areas of grey. Please refer to the original content linked in the post
     for all the details.`,
     improvingPatternsIntro: `These are my notes about improving basic programming patterns.
-    There might be mistakes, better solutions, and trade offs.`
+    There might be mistakes, better solutions, and trade offs.`,
+    learningIntro: `These are my notes from building projects. I research and build
+    side projects to learn various things I’m curious about. The explanations and code examples
+    have not been reviewed by anyone unless specified, and might therefore be incorrect.
+    I suggest to only use these as a simple guide and always cross reference with another source.`,
 };
